f9754b95a7bea6700e4579e2c7ffcf1777700b49
Thorgathis <nicolas.kopytin@gmail.com>
2022-07-12 14:36:38 +0300

    add embed

--- assets/script.js
+++ assets/script.js
@@ -9,14 +9,18 @@ function ThemeToggle() {
 
 function ThemeSet() {
     if (localStorage.getItem("theme") === "light") {
-        $("body").removeClass("backg-dark");   
-        $("nav").removeClass("navbar-dark bg-dark").addClass("navbar-light bg-light");
+        $("body").removeClass("backg-dark");
+        $("nav")
+            .removeClass("navbar-dark bg-dark")
+            .addClass("navbar-light bg-light");
         $(".text").removeClass("col-gray-white");
         $(".pbar").removeClass("pbar-bg");
         $(".card").removeClass("card-dark");
     } else {
-        $("body").addClass("backg-dark");   
-        $("nav").addClass("navbar-dark bg-dark").removeClass("navbar-light bg-light");
+        $("body").addClass("backg-dark");
+        $("nav")
+            .addClass("navbar-dark bg-dark")
+            .removeClass("navbar-light bg-light");
         $(".text").addClass("col-gray-white");
         $(".pbar").addClass("pbar-bg");
         $(".card").addClass("card-dark");
